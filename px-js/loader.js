ee4bfe22f36c02610421dc6f531db292a1a8c947
raven <rjcapiral.designer@gmail.com>
2025-07-09 18:24:40 +0800

    v 1.6
    Done: 4 Section (Desktop)
    Next: Section 5 and 6

--- px-js/loader.js
+++ px-js/loader.js
@@ -10,4 +10,17 @@ document.addEventListener('DOMContentLoaded', async function () {
   } catch (error) {
     console.error('Error loading components:', error);
   }
+
+  const mainImage = document.getElementById('px-img-0129');
+  const parentDiv = document.getElementById('0416-rwrapper');
+  if (mainImage.complete) {
+    applyImageHeight();
+  } else {
+    mainImage.onload = applyImageHeight;
+  }
+
+  function applyImageHeight() {
+    const height = mainImage.getBoundingClientRect().height;
+    parentDiv.style.height = `${height}px`;
+  }
 });
